fbd20ca433774e7ac62a93a9a2d931acf1915b0a
Madhur Tandon <madhur16053@iiitd.ac.in>
2017-01-28 16:04:13 +0530

    speechRecAI.py: Set Confidence Threshold
    Choose a custom minimum confidence level
    for improving accuracy of detection
    among a set of possible outcomes.
    
    Fixes https://github.com/amrandahvur/Smart-Mirror/issues/5

--- Modules/speechRecAI.py
+++ Modules/speechRecAI.py
@@ -1,29 +1,46 @@
 import speech_recognition as speechAI
 
 class SpeechAI(object):
-    def __init__(self,phrase="hello"):
-        self.phrase = phrase;
+    def __init__(self,threshold=0.30,phrase="hello"):
+        self.phrase = phrase
+        self.threshold = threshold
 
     def recognize(self,recognize,audio):
         spoken = None
+        transcript=""
         try:
-            spoken = recognize.recognize_google(audio)
-            print("YOU SAID "+spoken)
+            min_conf = 1
+            spoken = recognize.recognize_google(audio,key="AIzaSyAcalCzUvPmmJ7CZBFOEWx2Z1ZSn4Vs1gg",language = "en-IN",show_all=True)
+            print(spoken)
+            if spoken != []:
+                for i in spoken['alternative']:
+                    if i['confidence']<=min_conf and i['confidence']>=self.threshold:
+                        min_conf = i['confidence']
+                for i in spoken['alternative']:
+                    if i['confidence']==min_conf:
+                        transcript = i['transcript']
+                        print("YOU SAID "+i['transcript'])
+                        break
+            else:
+                print("Couldn't Understand")
         except speechAI.UnknownValueError:
             print("Couldn't Understand")
-        return spoken
+        except speechAI.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        return transcript
 
     def ears(self):
         micro_source = speechAI.Microphone()
         record = speechAI.Recognizer()
         with micro_source as source:
-            record.adjust_for_ambient_noise(source)
+            record.adjust_for_ambient_noise(source,duration=1)
+            record.dynamic_energy_threshold = True
             print("I am all Ears!")
             audio = record.listen(source)
         return record,audio
 
 if __name__=="__main__":
-    S = SpeechAI("ok mirror")
+    S = SpeechAI(0.30,"okay mirror")
     record,audio = S.ears()
     x = S.recognize(record,audio)
     if x==S.phrase:
