0fbcb750d83d9c7de1c86321fe1ed0593cca8487
Madhur Tandon <madhur16053@iiitd.ac.in>
2017-02-07 01:43:08 +0530

    speechRecAI.py: Lexicographical in Constructor
    Chooses Transcript that appears Lexicographically first
    from a set of transcripts with same confidence levels after
    setting the threshold.
    
    Used when Numbers are given a preference first.
    
    Closes https://github.com/amrandahvur/Smart-Mirror/issues/7

--- Modules/speechRecAI.py
+++ Modules/speechRecAI.py
@@ -1,28 +1,59 @@
 import speech_recognition as speechAI
 
 class SpeechAI(object):
-    def __init__(self,threshold=0.30,phrase="hello"):
+    def __init__(self,threshold=0.30,lex=False,phrase="hello"):
         self.phrase = phrase
+        self.lex = lex
         self.threshold = threshold
 
     def recognize(self,recognize,audio):
         spoken = None
         transcript=""
         try:
-            min_conf = 1
-            spoken = recognize.recognize_google(audio,key="AIzaSyAcalCzUvPmmJ7CZBFOEWx2Z1ZSn4Vs1gg",language = "en-IN",show_all=True)
-            print(spoken)
-            if spoken != []:
-                for i in spoken['alternative']:
-                    if i['confidence']<=min_conf and i['confidence']>=self.threshold:
-                        min_conf = i['confidence']
-                for i in spoken['alternative']:
-                    if i['confidence']==min_conf:
-                        transcript = i['transcript']
-                        print("YOU SAID "+i['transcript'])
-                        break
+            if(self.lex==False):
+                min_conf = 1
+                spoken = recognize.recognize_google(audio,key="AIzaSyAcalCzUvPmmJ7CZBFOEWx2Z1ZSn4Vs1gg",language = "en-IN",show_all=True)
+                print(spoken)
+                if spoken != []:
+                    for i in spoken['alternative']:
+                        if 'confidence' not in list(i.keys()):
+                            transcript = spoken['alternative'][0]['transcript']
+                            print("YOU SAID "+i['transcript'])
+                            return transcript
+                        elif i['confidence']<=min_conf and i['confidence']>=self.threshold:
+                            min_conf = i['confidence']
+                    for i in spoken['alternative']:
+                        if i['confidence']==min_conf:
+                            transcript = i['transcript']
+                            print("YOU SAID "+i['transcript'])
+                            break
+                else:
+                    print("Couldn't Understand")
             else:
-                print("Couldn't Understand")
+                min_conf = 1
+                spoken = recognize.recognize_google(audio,key="AIzaSyAcalCzUvPmmJ7CZBFOEWx2Z1ZSn4Vs1gg",language = "en-IN",show_all=True)
+                print(spoken)
+                outcomes = []
+                if spoken != []:
+                    for i in spoken['alternative']:
+                        if 'confidence' not in list(i.keys()):
+                            transcript = spoken['alternative'][0]['transcript']
+                            print("YOU SAID "+i['transcript'])
+                            return transcript
+                        elif i['confidence']<=min_conf and i['confidence']>=self.threshold:
+                            min_conf = i['confidence']
+                    for i in spoken['alternative']:
+                        if i['confidence']==min_conf:
+                            outcomes.append(i['transcript'])
+                    outcomes.sort()
+                    print(outcomes)
+                    transcript = outcomes[0]
+                    print("YOU SAID "+transcript)
+                            # transcript = i['transcript']
+                            # print("YOU SAID "+i['transcript'])
+                            # break
+                else:
+                    print("Couldn't Understand")
         except speechAI.UnknownValueError:
             print("Couldn't Understand")
         except speechAI.RequestError as e:
